0c78e9d0285473a5237fba3e67b194cb2e4588fe
nevol78@mail.ru <nevol78@mail.ru>
2022-05-28 00:46:00 +0300

    Task38

---
+++ Task38/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Задайте размер массива: ");
+int size = int.Parse(Console.ReadLine());
+int[] array = new int[size];
+
+var newRnd = new Random();
+Console.Write($"Массив: ");
+for (int i = 0; i < size; i++)
+{
+array[i] = newRnd.Next(0, 20);
+Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+
+int max = array[0];
+int min = array[0];
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > max) max = array[i];
+    if (array[i] < min) min = array[i];
+}
+Console.Write("Разница между максимальным и минимальным элементом = ");
+Console.Write(max - min);
